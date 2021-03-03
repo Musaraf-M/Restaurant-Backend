de077e5c99c40a879cb5bd50c06faa48697a8ed2
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-03-03 15:46:31 +0530

    (add) app:Basic structure

---
+++ src/app.js
@@ -0,0 +1,38 @@
+// Imports
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Express
+const app = express();
+
+// Cors 
+app.use(cors());
+
+// dotenv
+dotenv.config({ path: 'src/.env' });
+
+// Connect Mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_CONNECT, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+    })
+    .then((res) => console.log("Connected to DB"))
+    .catch((err) => console.log(err));
+
+//Middlewares
+app.use(express.json());
+
+//Mock Request
+app.get("/", (req,res) => {
+    res.send("<h1>API is working</h1>");
+})
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Listen
+app.listen(PORT, () => {
+    console.log(`Server up and running at ${PORT}`);
+});
