1544017436ca6a9c04ab12545ce89e085ecb745a
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-03-03 16:09:43 +0530

    (fix) validation: login,user validations

--- src/services/validation.js
+++ src/services/validation.js
@@ -20,3 +20,31 @@ const registerValidation = (data) => {
 
     return schema.validate(data);
 };
+
+// Login Validation
+const loginValidation = (data) => {
+    const schema = joi.object({
+        email: joi.string().min(6).email(),
+        password: joi
+            .string()
+            .pattern(
+                new RegExp(
+                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$"
+                )
+            )
+            .error(() => {
+                return new Error("");
+            }),
+    });
+
+    return schema.validate(data);
+};
+
+// User data validation
+const userValidation = (data) => {
+    const schema = joi.object({
+        name: joi.string().min(6).required(),
+    });
+
+    return schema.validate(data);
+};
